43ce75fce6c6afe92828617666afc33cb79752bc
stevano nathan hamtoyo <stevano27nathan@gmail.com>
2025-12-08 20:23:30 +0700

    Update createitem

--- modules/items/routes/createItem.py
+++ modules/items/routes/createItem.py
@@ -6,12 +6,12 @@ from sqlalchemy.orm import Session
 from database import get_db
 from modules.items.schema.models import MentalHealthResponse
 from modules.items.schema.schemas import MentalHealthCreate, MentalHealthOut
+from modules.items.scripts.cleaning import normalisasi  #tambah ini
 
 router = APIRouter(
     prefix="/mental-health",
     tags=["mental_health_create"],
 )
-
 
 @router.post(
     "/responses",
@@ -22,8 +22,12 @@ def create_response(payload: MentalHealthCreate, db: Session = Depends(get_db)):
     """
     Menambahkan satu data baru ke tabel mental_health_responses.
     """
+    #langsung normalisasi teks input
+    clean_text = normalisasi(payload.statement)
+
     obj = MentalHealthResponse(
         statement=payload.statement,
+        clean_statement=clean_text,   #simpan teks bersih juga
         status=payload.status,
     )
     db.add(obj)
